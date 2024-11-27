9c82c0d24e73c634b39af2db7b29a3993eba5f07
Neckros1821 <alex.drimis@hotmail.com>
2024-11-27 15:05:58 +0100

    [+] create game-manager
    creation of the project with spring boot initializer

---
+++ game-manager/src/main/java/com/example/game_manager/GameManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameManagerApplication.class, args);
+	}
+
+}
---
+++ game-manager/src/test/java/com/example/game_manager/GameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
